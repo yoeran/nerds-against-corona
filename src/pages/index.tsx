de00f7e937da238c37605ef5befe0cb89ce12494
Yoeran <contact@yoeran.nl>
2020-03-23 11:01:04 +0100

    Add og:image meta-tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,14 @@ import { Team } from "../components/Team"
 import { Sponsors } from "../components/Sponsors"
 import { Explainer } from "../components/Explainer"
 
+import headerImage from "../images/nerds-against-corona-header.png"
+
 const IndexPage = () => (
   <Layout>
-    <Seo title="Hulp nodig?" />
+    <Seo
+      title="Hulp nodig?"
+      meta={[{ property: "og:image", content: headerImage }]}
+    />
 
     <Explainer />
     <Team />
